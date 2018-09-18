1c6f1c76a07628dfff3cc57c7a6640d491f274d5
Edwin Flores <edflores28@gmail.com>
2018-09-18 15:46:09 -0400

    New constants file

---
+++ Dissasembler/constants.py
@@ -0,0 +1,5 @@
+opcodes = {0x31: None, 0x01: None, 0x55: None, 0x89: None, 0x52: None, 0x51: None,
+            0xB8: None, 0x8B: None, 0x5A: None, 0x59: None, 0x5D: None, 0xC2: None}
+
+registers = {0: 'eax', 1: 'ecx', 2: 'edx', 3: 'ebx', 4:'esp', 5:'ebp', 6: 'esi', 7: 'edi'}
+
