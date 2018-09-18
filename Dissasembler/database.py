217616844b1b3851981d892030d000bf6c4f0b95
Edwin Flores <edflores28@gmail.com>
2018-09-18 15:45:43 -0400

    Current checkpoint

--- Dissasembler/database.py
+++ Dissasembler/database.py
@@ -1,12 +1,20 @@
+import copy
+
 class Database:
     def __init__(self):
         '''
         Initialization
         '''
         self.database = []
+        self.counts = []
+        self.total_bytes = -1
+        self.formatted = []
 
     def insert(self, byte):
         '''
-        Inserts a byte to the instruction database
+        Inserts a byte to the database
         '''
-        self.database.append(byte)
+        self.database.append(copy.deepcopy(byte))
+
+    def format(self):
+        print(self.database)
--- Dissasembler/main.py
+++ Dissasembler/main.py
@@ -1,5 +1,7 @@
 import argparse
 import os.path
+import database
+import constants
 
 def open_file(parse, file):
     '''
@@ -11,6 +13,9 @@ def open_file(parse, file):
     else:
         return open(file, 'rb')
 
+'''
+Main application
+'''
 # Create a parser for the binary file
 parser = argparse.ArgumentParser(description="Dissasembler")
 parser.add_argument("-f", dest="filename", required=True, help="Machine code file", type=lambda x: open_file(parser, x))
@@ -18,10 +23,27 @@ parser.add_argument("-f", dest="filename", required=True, help="Machine code fil
 # Read user inputs
 args = parser.parse_args()
 
+# Instantiate the database
+machine_code = database.Database()
+line = []
+found_opcode = False
 while True:
     current_byte = args.filename.read(1)
-    if not current_byte:
+    try:
+        val = ord(current_byte)
+    except TypeError:
+        print("Error")
+        machine_code.insert(line)
         break
-    val = ord(current_byte)
-    print(val)
-    print("%#5X"% (val))
+    if val in constants.opcodes:
+        # If there are bytes in the line then
+        # insert the list in the database
+        # and clear the line list
+        if len(line) > 0:
+            #print(hex(line))
+            machine_code.insert(line)
+            line.clear()
+    # Add the byte to the line
+    line.append(val)
+
+machine_code.format()
